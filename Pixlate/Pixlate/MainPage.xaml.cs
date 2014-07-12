35432c771e0fc7a45d153f2572f92aff7831f46f
Henrik Lábadi <hlabadi@hotmail.com>
2014-07-12 21:05:34 +0200

    Pixlate initial commit
    Add Pixlate Effect
    Add Pixlate sample WP8 app

---
+++ Pixlate/Pixlate/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+using Nokia.Graphics.Imaging;
+using Pixlate.Resources;
+using System.Windows.Media.Imaging;
+
+namespace Pixlate
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            PhotoChooserTask pct = new PhotoChooserTask();
+            pct.Completed += pct_Completed;
+            pct.Show();
+        }
+
+        async void pct_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult != TaskResult.OK || e.ChosenPhoto == null)
+                return;
+
+            try
+            {
+                // Create a source to read the image from PhotoResult stream
+                using (var source = new StreamImageSource(e.ChosenPhoto))
+                {
+                    using (var effect = new PixlateEffect(source))
+                    {
+                        //var target = new WriteableBitmap(1920, 1080);
+                        var target = new WriteableBitmap((int)ResultImage.ActualWidth, (int)ResultImage.ActualHeight);
+                        // Create a new renderer which outputs WriteableBitmaps
+                        using (var renderer = new WriteableBitmapRenderer(effect, target))
+                        {
+                            // Render the image with the filter(s)
+                            await renderer.RenderAsync();
+                            // Set the output image to Image control as a source
+                            ResultImage.Source = target;
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                return;
+            }
+        }
+
+    }
+}
---
+++ Pixlate/Pixlate/PixlateEffect.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nokia.Graphics.Imaging;
+
+namespace Pixlate
+{
+    public class PixlateEffect : CustomEffectBase
+    {
+        public PixlateEffect(IImageProvider provider)
+            : base(provider)
+        {
+
+        }
+        private uint ResultPixel(uint sourcePixel)
+        {
+            var pixel = sourcePixel;
+            uint blue = pixel & 0x000000ff; // blue color component
+            uint green = (pixel & 0x0000ff00) >> 8; // green color component
+            uint red = (pixel & 0x00ff0000) >> 16; // red color component
+            return 0xdd000000 | blue | (green << 8) | (red << 16); // we return the original pixel, only slightly darker
+
+        }
+        protected override void OnProcess(PixelRegion sourcePixelRegion, PixelRegion targetPixelRegion)
+        {
+            var sourcePixels = sourcePixelRegion.ImagePixels;
+            var targetPixels = targetPixelRegion.ImagePixels;
+            var row = 0;
+            sourcePixelRegion.ForEachRow((index, width, position) =>
+            {
+                if (row % 2 == 0)
+                {
+                    for (int y = 0; y < width; ++y, ++index)
+                    {
+                        targetPixels[index] = ResultPixel(sourcePixels[index]);
+                    }
+                }
+                else
+                {
+                    for (int x = 0; x < width; ++x, ++index)
+                    {
+                        if (x % 2 == 0)
+                            targetPixels[index] = ResultPixel(sourcePixels[index]);
+                        else
+                            targetPixels[index] = sourcePixels[index];
+                    }
+                }
+                row++;
+
+            });
+        }
+    }
+}
